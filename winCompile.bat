720d2c6528a1fd49fbb173ed89fd8f4c5a96d58f
Ricard Marxer <email@ricardmarxer.com>
2008-08-09 14:22:49 +0200

    removed old scripts that are replaced by the Makefile

--- winCompile.bat
+++
@@ -1,6 +0,0 @@
-cd C:\Down\=Master\Geomerative
-javac -target 1.1 -classpath "C:\Down\=Software\processing-0098\lib\core.jar;C:\Down\=Master\Geomerative\external\batikfont.jar" -source 1.3 -d .\library .\geomerative\*.java
-cd library
-jar -cf .\geomerative.jar .\geomerative
-cd ..
-copy .\library\geomerative.jar "..\..\=Software\processing-0098\libraries\geomerative\library"
--- winGenerateDoc.bat
+++
@@ -1,1 +0,0 @@
-javadoc -classpath "C:\Down\=Software\processing-0091\lib\core.jar" -doclet prodoc.StartDoclet -docletpath .\prodoc -sourcepath  . rgeom
